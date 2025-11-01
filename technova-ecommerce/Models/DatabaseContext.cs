1ef6b58c73d73bf9aa8242e634cb94523aa7dc39
Mustafa Hassan <mhassan@corcentric.com>
2025-11-01 11:04:45 +0500

    Lecture 7

---
+++ technova-ecommerce/Models/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using technova_ecommerce.Models.Entities;
+
+namespace technova_ecommerce.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ technova-ecommerce/Models/Entities/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace technova_ecommerce.Models.Entities
+{
+    [Table("Customer")]
+    public class Customer
+    {
+        [Column("customer_id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerId { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("password")]
+        public string Password { get; set; }
+
+        [Column("address")]
+        public string Address { get; set; }
+
+        [Column("phone_number")]
+        public string PhoneNumber { get; set; }
+    }
+}
