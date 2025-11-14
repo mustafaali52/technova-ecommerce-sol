15e8f62b391553a8033e49395b240f1e8d40db1b
Mustafa Hassan <mhassan@corcentric.com>
2025-11-14 11:48:49 +0500

    Lecture 9 - Check-In

--- technova-ecommerce/Models/Entities/Customer.cs
+++ technova-ecommerce/Models/Entities/Customer.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Models.Entities
@@ -17,21 +18,28 @@ namespace Models.Entities
         public int CustomerId { get; set; }
 
         [Column("first_name")]
+        [Display (Name = "Name")]
         public string FirstName { get; set; }
 
         [Column("last_name")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Column("email")]
+        [Display(Name = "Email")]
+        //[Validation(Regex = "")]
         public string Email { get; set; }
 
         [Column("password")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [Column("address")]
+        [Display(Name = "Address")]
         public string Address { get; set; }
 
         [Column("phone_number")]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
     }
 }
