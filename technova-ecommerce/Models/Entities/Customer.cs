e403652e452b791d0059c1aa0c8b236747f49b1f
Mustafa Hassan <mhassan@corcentric.com>
2025-11-16 12:18:51 +0500

    Class work lecture 9

--- technova-ecommerce/Models/Entities/Customer.cs
+++ technova-ecommerce/Models/Entities/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace technova_ecommerce.Models.Entities
 {
@@ -12,21 +13,34 @@ namespace technova_ecommerce.Models.Entities
         public int CustomerId { get; set; }
 
         [Column("first_name")]
+        [Display(Name = "Name")]
         public string FirstName { get; set; }
 
         [Column("last_name")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Column("email")]
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
 
         [Column("password")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [Column("address")]
+        [Display(Name = "Address")]
         public string Address { get; set; }
 
         [Column("phone_number")]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+
+        //[Column("fax_number")]
+        //[AllowNull]
+        //public string? FaxNumber { get; set; }
+
+        //[Column("gender")]
+        //public string Gender { get; set; }
     }
 }
