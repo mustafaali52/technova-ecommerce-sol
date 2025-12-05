1270d0e88b9d65ce020e5be3fa98f22c46939db4
Mustafa Hassan <mhassan@corcentric.com>
2025-12-05 21:18:27 +0500

    Products Implementation

---
+++ technova-ecommerce/Models/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace technova_ecommerce.Models.Entities
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("product_id")]
+        public int ProductId { get; set; }
+        [Column("product_name")]
+        public string ProductName { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+        [Column("stock_price")]
+        public decimal StockPrice { get; set; }
+        [Column("quantity")]
+        public int Quantity { get; set; }
+    }
+}
